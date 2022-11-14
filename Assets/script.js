4b5ff90722f16c7bbc028ca5367369f84b87b046
[Andres Perez] <mandresperez.uk@gmail.com>
2022-11-13 17:28:08 -0700

    Finished adding all of the API calls and functions in js

--- Assets/script.js
+++ Assets/script.js
@@ -1,17 +1,52 @@
 var userInput = document.getElementById("user-input");
 var userForm = document.getElementById("form-sbt");
-var weatherapi = "http://api.openweathermap.org/geo/1.0/direct?q=denver&appid=0979296881ad793a95b3a80e818231f4"
+var apiKey = "d91f911bcf2c0f925fb6535547a5ddc9"
+var dayForecast = document.getElementById("current-forecast")
+
+function fetchCoordinates(city) {
+    console.log(city)
+    var apiCall = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey;
+
+    fetch(apiCall)
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(data) {
+            var lat = data[0].lat
+            var lon = data[0].lon
+            fetchWeather (lat, lon) 
+        });
+}
 
 // Function is responsible for making api call with the user search term
-function fetchWeather (city) {
-    fetch()
+function fetchWeather (lat, lon) {
+    var weatherApi = "https://api.openweathermap.org/data/2.5/forecast?"
+    var apiCall = weatherApi + "lat=" + lat + "&lon=" + lon + "&units=imperial" + "&appid=" + apiKey
+    fetch(apiCall)
+    .then(function(response) {
+    return response.json()
+    })
+    .then(function(data) {
+        var h1El = document.createElement("h1")
+        h1El.textContent = data.list[0].main.temp
+        dayForecast.append(h1El)
+    })
 }
 
-// This fucntion is responsible for form submission by capturing userInput
-function handleFormSubmit(e) {
+function renderDayForecast () {
+
+}
+
+// This function is responsible for form submission by capturing userInput
+var handleFormSubmit =  function(e) {
     e.preventDefault()
     var input = userInput.value
-    fetchWeather(input)
+    fetchCoordinates(input)
 }
 
-userForm.addEventListener("submit", handleFormSubmit())
+userForm.addEventListener("submit", handleFormSubmit)
+
+//local storage
+//create an empty array
+//push that value(city name) to the array
+//
